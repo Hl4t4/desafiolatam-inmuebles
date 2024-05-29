ce5c2680549abb83d5f7ce2f3d2b88ccf2a8433e
Jose Latapiatt <jose.latapiatt@proton.me>
2024-05-29 05:00:07 -0400

    Base Hito 3

--- inmuebles/web/views.py
+++ inmuebles/web/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import Usuario, Inmueble
+
+def index_view(request):
+    return render(request, "index.html", {})
 
 # Create your views here.
