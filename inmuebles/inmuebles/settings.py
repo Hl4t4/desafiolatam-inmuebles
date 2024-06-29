dc2802dd908345363afc67208a4bf416bb8b36ed
Jose Latapiatt <jose.latapiatt@proton.me>
2024-06-28 23:19:50 -0400

    Env

--- inmuebles/inmuebles/settings.py
+++ inmuebles/inmuebles/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Load environment variables from .env file
-env_path = BASE_DIR / '.env'
-load_dotenv(dotenv_path=env_path)
+#env_path = BASE_DIR / '.env'
+#load_dotenv(dotenv_path=env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
